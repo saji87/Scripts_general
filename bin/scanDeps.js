39f5f36816838442180ee016fca18e535630b13d
Brad Robinson <brobinson@toptensoftware.com>
2019-12-21 14:23:18 +1100

    Updated dep scanning to return topoligically sorted list

--- bin/scanDeps.js
+++ bin/scanDeps.js
@@ -163,10 +163,16 @@ function scanDeps(baseDir, rootFiles, options)
         }
     }
 
+    // A map of filename to array of other files that it depends on
+    var fileDepMap = {};
+
     // Scan for dependencies
-    var processedDeps = {};
     for (let i=0; i<allFiles.length; i++)
     {
+        // Create an array of dependencies for this file
+        var fileDeps = [];
+        fileDepMap[allFiles[i]] = fileDeps;
+
         // Find the dependencies for this file
         var deps = findDeps(allFiles[i]);
         for (let j=0; j<deps.length; j++)
@@ -174,25 +180,72 @@ function scanDeps(baseDir, rootFiles, options)
             // Get the depenency's name
             var dep = deps[j];
 
-            // Already processed?
-            if (processedDeps[dep])
-                continue;
-            processedDeps[dep] = true;
-
             // Try to resolve it
             var resolved = resolveDepFile(dep, allFiles[i], depPath, options);
             if (resolved)
             {
+                // Add to map for this file
+                fileDeps.push(resolved);
+
+                // If it's a file we haven't seen, add it to the list still
+                // to be processed.
                 if (allFiles.indexOf(resolved) < 0)
                     allFiles.push(resolved);
             }
         }
     }
 
-    // Convert all paths back to relative paths to the base directory
-    allFiles = allFiles.map(x => path.relative(baseDir, x));
+    // Build a sorted list where all files appear after any files they
+    // depend on.  (circular references shouldn't happen but handle it anyway)
+    // ie: Topological sort.  Use Kahn's algorithm for simplicity
 
-    return allFiles;
+    // L ← Empty list that will contain the sorted elements
+    var L = [];
+
+    // S ← Set of all nodes with no incoming edge
+    var S = allFiles.filter(x => fileDepMap[x].length == 0);
+
+    // while S is non-empty do
+    while (S.length != 0)
+    {
+        // remove a node n from S
+        var n = S.shift();
+
+        // add n to tail of L
+        L.push(n);
+
+        // for each node m with an edge e from n to m do
+        for (let i=0; i<allFiles.length; i++)
+        {
+            var m = allFiles[i];
+            var mdeps = fileDepMap[m];
+            var index = mdeps.indexOf(n);
+
+            // remove edge e from the graph
+            if (index >= 0)
+            {
+                mdeps.splice(index, 1);
+
+                // if m has no other incoming edges then
+                if (mdeps.length == 0)
+                {
+                    //insert m into S
+                    S.push(m);
+                }
+            }
+        }
+    }
+
+    // Add any missing files to L (means there was a circular reference)
+    for (let i=0; i<allFiles.length; i++)
+    {
+        if (L.indexOf(allFiles[i]) < 0)
+            L.push(allFiles[i])
+    }
+
+    // Convert all paths back to relative paths to the base directory
+    L = L.map(x => path.relative(baseDir, x));
+    return L;
 }
 
 //var files = scanDeps(
